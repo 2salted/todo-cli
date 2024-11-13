36060b5076750545ec4775fd5e6a0b7cec5e690c
2salted <noamgdev@gmail.com>
2024-11-13 15:48:40 -0100

    added get latest id from todo list

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <ios>
 #include <iostream>
+#include <optional>
 #include <string>
 #include <tuple>
 #include <vector>
@@ -16,22 +17,43 @@ using std::string;
 using std::tuple;
 using std::vector;
 
-void createTodo(string const &title, i32 id, i32 priority) {
-  ofstream file(std::string(std::getenv("HOME")) + "/.cache/todo-cli/todo.txt",
+i32 getLatestId() {
+  ifstream file(string(std::getenv("HOME")) + "/.cache/todo-cli/todo.txt");
+  if (!file) {
+    return 1;
+  }
+
+  i32 id, priority;
+  string title;
+  i32 lastId;
+
+  while (file >> id >> priority) {
+    getline(file >> std::ws, title);
+    lastId = id;
+  }
+  file.close();
+  return lastId;
+}
+
+void createTodo(string const &title, i32 priority) {
+  i32 id;
+  ofstream file(string(std::getenv("HOME")) + "/.cache/todo-cli/todo.txt",
                 std::ios::app);
-  file << id << " " << priority << " " << title << endl;
+  id = getLatestId();
+  file << id + 1 << " " << priority << " " << title << endl;
   file.close();
 }
 
 vector<tuple<i32, i32, string>> listTodo() {
   vector<tuple<i32, i32, string>> todoList;
-  ifstream file(std::string(std::getenv("HOME")) + "/.cache/todo-cli/todo.txt");
+  ifstream file(string(std::getenv("HOME")) + "/.cache/todo-cli/todo.txt");
   i32 id, priority;
   string title;
   while (file >> id >> priority) {
     getline(file >> std::ws, title);
     todoList.emplace_back(id, priority, title);
   }
+  file.close();
   sort(todoList.begin(), todoList.end(), [](auto const &a, auto const &b) {
     return std::get<1>(a) > std::get<1>(b);
   });
@@ -40,7 +62,7 @@ vector<tuple<i32, i32, string>> listTodo() {
 
 void closeTodo(i32 idToRemove) {
   vector<tuple<i32, i32, string>> todoList;
-  ifstream file(std::string(std::getenv("HOME")) + "/.cache/todo-cli/todo.txt");
+  ifstream file(string(std::getenv("HOME")) + "/.cache/todo-cli/todo.txt");
   i32 id, priority;
   string title;
 
@@ -52,8 +74,7 @@ void closeTodo(i32 idToRemove) {
   }
   file.close();
 
-  ofstream outFile(std::string(std::getenv("HOME")) +
-                       "/.cache/todo-cli/todo.txt",
+  ofstream outFile(string(std::getenv("HOME")) + "/.cache/todo-cli/todo.txt",
                    std::ios::trunc);
   for (const auto &[id, priority, title] : todoList) {
     outFile << id << " " << priority << " " << title << endl;
@@ -61,27 +82,4 @@ void closeTodo(i32 idToRemove) {
   outFile.close();
 }
 
-int main() {
-  createTodo("Buy groceries", 1, 5);
-  createTodo("Study for exam", 2, 8);
-  createTodo("Workout", 3, 6);
-
-  std::cout << "To-do list before removal:" << endl;
-  vector<tuple<i32, i32, string>> todos = listTodo();
-  for (const auto &[id, priority, title] : todos) {
-    std::cout << "ID: " << id << ", Priority: " << priority
-              << ", Title: " << title << endl;
-  }
-
-  i32 idToRemove = 2;
-  closeTodo(idToRemove);
-
-  std::cout << "\nTo-do list after removing ID " << idToRemove << ":" << endl;
-  todos = listTodo();
-  for (const auto &[id, priority, title] : todos) {
-    std::cout << "ID: " << id << ", Priority: " << priority
-              << ", Title: " << title << endl;
-  }
-
-  return 0;
-}
+int main() { return 0; }
